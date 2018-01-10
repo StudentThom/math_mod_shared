99e3339fe743044f66da77721f16e9555bf76f9d
pneppelenbroek <35293517+pneppelenbroek@users.noreply.github.com>
2018-01-10 11:48:15 +0100

    Add files via upload

---
+++ workshopmathmodel.R
@@ -0,0 +1,18 @@
+setwd("C:/Users/Paulien/Downloads")
+load('allp.Rdata')
+
+allp$p1
+
+# histograms
+par(mfrow=c(2,2))
+
+
+for (i in seq(1,length(allp))){
+  hist(allp[[i]],
+       main=paste("Hist of pvector", i),xlab = "p-value",prob=T)
+}
+
+for (i in seq(1,length(allp))){
+  plot(sort(allp[[i]]), main=paste("p-values of pvector"),
+       xlab="", ylab="sorted p-values",col="blue", type="l")
+}
